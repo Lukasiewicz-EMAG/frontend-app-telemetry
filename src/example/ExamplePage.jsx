c6d20e5486ab25dbab4f4ff9be8868f2ba7771e6
Marek Hermansa <marekhermansa@gmail.com>
2024-09-23 20:22:21 +0200

    Add task list fetching

--- src/example/ExamplePage.jsx
+++ src/example/ExamplePage.jsx
@@ -1,12 +1,56 @@
+import './index.scss';
 import { Container } from '@openedx/paragon';
+import React, { useEffect, useState } from 'react';
 
-const ExamplePage = () => (
-  <main>
-    <Container className="py-5">
-      <h1>Example Page</h1>
-      <p>Hello world!</p>
-    </Container>
-  </main>
-);
+const ExamplePage = () => {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("http://tools.dev.cudzoziemiec.emag.lukasiewicz.local/cudserver/task/list?type=cudmath", {
+        method: 'GET',
+        headers: {
+          'accept': 'application/json',
+        },
+      });
+      const data = await response.json();
+      setTasks(data);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <main>
+      <Container className="py-5">
+        <h1>Example Page</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Task ID</th>
+              <th>Type</th>
+              <th>Category</th>
+              <th>Language</th>
+              <th>Title</th>
+              <th>Description</th>
+            </tr>
+          </thead>
+          <tbody>
+            {tasks.map(task => (
+              <tr key={task.taskId}>
+                <td>{task.taskId}</td>
+                <td>{task.type}</td>
+                <td>{task.category}</td>
+                <td>{task.lang}</td>
+                <td>{task.title}</td>
+                <td dangerouslySetInnerHTML={{ __html: task.description }} />
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </Container>
+    </main>
+  );
+};
 
 export default ExamplePage;

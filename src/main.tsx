2d6be5b27913ebf534509cb6aee13df72d0a4e72
pawel.lizner <pawel.lizner@emag.lukasiewicz.gov.pl>
2024-09-26 15:43:20 +0200

    CUD-1836 - test

--- src/main.tsx
+++ src/main.tsx
@@ -1,45 +1,37 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-// import { initialize, subscribe, APP_READY, APP_INIT_ERROR } from '@edx/frontend-platform';
-// import { AppProvider, ErrorPage } from '@edx/frontend-platform/react';
-// import './index.css';
-// import { Suspense } from 'react';
-// import { IntlProvider } from 'react-intl';
-// import { messages } from './i18n/index.ts';
-// import { General } from './pages/General/General.tsx';
-// import { Layout } from './modules/Layout.tsx';
+import { initialize, subscribe, APP_READY, APP_INIT_ERROR } from '@edx/frontend-platform';
+import { AppProvider, ErrorPage } from '@edx/frontend-platform/react';
+import './index.css';
+import { Suspense } from 'react';
+import { IntlProvider } from 'react-intl';
+import { messages } from './i18n/index.ts';
+import { General } from './pages/General/General.tsx';
+import { Layout } from './modules/Layout.tsx';
 
-ReactDOM.render(
-  <div><p>test</p></div>,
-  document.getElementById('root')
-);
+subscribe(APP_READY, () => {
+  ReactDOM.render(
+    <AppProvider >
+      <IntlProvider messages={messages.pl} locale="pl-PL" defaultLocale="pl">
+        <Suspense fallback={null}>
+          <Layout>
+            <General />
+          </Layout>
+        </Suspense>
+      </IntlProvider>
+    </AppProvider>,
+    document.getElementById('root')
+  );
+});
 
+subscribe(APP_INIT_ERROR, (error: Error) => {
+  console.error('APP_INIT_ERROR', error);
+  ReactDOM.render(<ErrorPage message={error.message} />, document.getElementById('root'));
+});
 
-// subscribe(APP_READY, () => {
-//   ReactDOM.render(
-//     <AppProvider >
-//       <div><p>test</p></div>
-//       {/* //   <IntlProvider messages={messages.pl} locale="pl-PL" defaultLocale="pl">
-//     //     <Suspense fallback={null}>
-//     //       <Layout>
-//     //         <General />
-//     //       </Layout>
-//     //     </Suspense>
-//     //   </IntlProvider> */}
-//     </AppProvider>,
-//     document.getElementById('root')
-//   );
-// });
-
-// subscribe(APP_INIT_ERROR, (error: Error) => {
-//   console.error('APP_INIT_ERROR', error);
-//   ReactDOM.render(<ErrorPage message={error.message} />, document.getElementById('root'));
-// });
-
-// initialize({
-//   messages: messages,
-//   requireAuthenticatedUser: true,
-//   hydrateAuthenticatedUser: true,
-//   locale: 'pl-PL',
-//   availableLocales: ['en', 'pl-PL'],
-// });
+initialize({
+  messages: messages,
+  requireAuthenticatedUser: true,
+  hydrateAuthenticatedUser: true,
+  locale: 'pl-PL',
+  availableLocales: ['en', 'pl-PL'],
+});

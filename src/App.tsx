66b3bbd05785e93275ff42bdfaa855198692b0ad
pawel.lizner <pawel.lizner@gmail.com>
2024-10-10 09:19:42 +0200

    CUD-1836 - routing

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,9 @@ export type RouteType = {
 
 function App() {
   const defaultRoutes: RouteType[] = [
-    { path: '/demo', element: <General /> },
-    { path: '/demo/referral', element: <Referral /> },
-    { path: '/demo/details', element: <Details /> },
+    { path: '/demo/cud-inf', element: <General /> },
+    { path: '/demo/cud-inf/referral', element: <Referral /> },
+    { path: '/demo/cud-inf/details', element: <Details /> },
     { path: '/demo/cud-math', element: <General /> },
     { path: '/demo/cud-math/referral', element: <Referral /> },
     { path: '/demo/cud-math/details', element: <CudMathDetails /> },

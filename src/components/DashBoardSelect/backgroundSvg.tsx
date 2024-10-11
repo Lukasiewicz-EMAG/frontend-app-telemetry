9d5505e0c2cd79a08c79a239e5d8121660fcc45f
pawel.lizner <pawel.lizner@gmail.com>
2024-10-11 08:58:47 +0200

    CUD-1836 - responsive

--- src/components/DashBoardSelect/backgroundSvg.tsx
+++ src/components/DashBoardSelect/backgroundSvg.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
 
-const BackgroundSvg = ({ className }) => (
+
+const BackgroundSvg = ({ className }: { className: string }) => (
     <svg
         xmlns="http://www.w3.org/2000/svg"
         width="734"

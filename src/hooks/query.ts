b93f546f8bac692dbb029f0ac773c75786181e62
pawel.lizner <pawel.lizner@gmail.com>
2024-10-29 11:27:32 +0100

    CUD-2098 - cookie test

--- src/hooks/query.ts
+++ src/hooks/query.ts
@@ -3,11 +3,6 @@ import axios, { AxiosError, AxiosResponse } from 'axios';
 import { jwtDecode } from 'jwt-decode';
 import { useState, useEffect } from 'react';
 import { getCookie } from '../lib/utils';
-import { getConfig } from '@edx/frontend-platform';
-import {
-  fetchAuthenticatedUser,
-  getAuthenticatedHttpClient
-} from '@edx/frontend-platform/auth';
 
 interface UserTokenPayload {
   sub: string;
@@ -55,7 +50,15 @@ const useAuthToken = (): string | null => {
         return savedToken;
       }
     }
-    return getCookie('edx-jwt-cookie-header-payload');
+    const payload = getCookie('edx-jwt-cookie-header-payload');
+    console.log('edx-jwt-cookie-header-payload', payload)
+    const signature = getCookie('edx-jwt-cookie-signature');
+    console.log('edx-jwt-cookie-signature', signature)
+  if (!payload || !signature) return null;
+  
+    const authToken = `${payload}.${signature}`;
+    console.log('authToken', authToken)
+  return authToken
   });
 
   useEffect(() => {
@@ -90,57 +93,29 @@ const setAuthToken = (newToken: string) => {
   queryClient.setQueryData(['authToken'], newToken);
 };
 
+const useGetData = <T,>(url: string, enabled: boolean = true): UseQueryResult<T, AxiosError> => {
+  const token = useAuthToken();
 
-// const useGetData = <T,>(url: string, enabled: boolean = true) => {
-//   const token = useAuthToken();
-
-//   return useQuery<T, AxiosError>(
-//     [url],
-//     async () => {
-//       if (!token) {
-//         throw new Error('Token is not available');
-//       }
-
-//       const { data, status } = await getAuthenticatedHttpClient().get<T>(url, {
-//         headers: {
-//           Authorization: `Bearer ${token}`,
-//         },
-//         baseURL: `https://tools.dev.cudzoziemiec.emag.lukasiewicz.local/telemetry-dashboard-api`,
-//         withCredentials: true,
-//       });
-      
-//       if (status !== 200) {
-//         throw new Error(`Error: Received status code ${status}`);
-//       }
-//       return data;
-//     },
-//     {
-//       enabled: !!token && enabled,
-//     }
-//   );
-// };
-const useGetData = <T,>(url: string, enabled: boolean = true) => {
   return useQuery<T, AxiosError>(
     [url],
     async () => {
-      const authenticatedUser = await fetchAuthenticatedUser(); 
-      console.log('authenticatedUser', authenticatedUser)
-      const authClient = getAuthenticatedHttpClient();
-      console.log('authClient', authClient);
-      const { data, status } = await authClient.get(`https://tools.dev.cudzoziemiec.emag.lukasiewicz.local/telemetry-dashboard-api` + url);
-      console.log('data statis', data, status);
-      
-      if (status !== 200) {
-        throw new Error(`Error: Received status code ${status}`);
+      if (!token) {
+        throw new Error('Token is not available');
       }
-      return data;
+      const response: AxiosResponse<T> = await axios.get(url, {
+        headers: {
+          Authorization: token,
+        },
+        baseURL: `https://tools.dev.cudzoziemiec.emag.lukasiewicz.local/telemetry-dashboard-api`,
+        withCredentials: true,
+      });
+      return response.data;
     },
     {
-      enabled: enabled,
+      enabled: !!token && enabled,
     }
   );
 };
-
 
 const usePostData = <T, B>(url: string): UseMutationResult<T, AxiosError, B> => {
   const token = useAuthToken();

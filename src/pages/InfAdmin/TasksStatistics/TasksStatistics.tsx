1a655130e87909d36f58ffdfd017c8cd836f170c
pawel.lizner <pawel.lizner@gmail.com>
2024-12-16 08:26:05 +0100

    CUD-2413 - new admin endpoints

--- src/pages/InfAdmin/TasksStatistics/TasksStatistics.tsx
+++ src/pages/InfAdmin/TasksStatistics/TasksStatistics.tsx
@@ -11,6 +11,7 @@ export const TasksStatistics = () => {
 
     return (
         <div className='mt-4 mx-0 md:ml-12 md:mr-12 lg:ml-16 lg:mr-16'>
+
             <h1 className="text-2xl font-bold">{intl.formatMessage({ id: 'admin_inf.general_statistics' })}</h1>
             <OldSelectionProvider endpoint='/admin/course_list' >
                 <div className="mt-4 grid grid-cols-1 gap-4">
